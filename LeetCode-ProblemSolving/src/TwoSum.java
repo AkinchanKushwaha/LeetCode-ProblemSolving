dfb880e588036c8f70cff9f11c7159ed57030651
AkinchanKushwaha <akinchandroger@gmail.com>
2020-10-17 08:17:53 +0530

    LeetCode

--- LeetCode-ProblemSolving/src/TwoSum.java
+++ LeetCode-ProblemSolving/src/TwoSum.java
@@ -1,4 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class TwoSum {
-
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hm  = new HashMap<>();
+        for(int i = 0 ; i<nums.length ; i++) {
+        	int c = target- nums[i];
+        	if(hm.containsKey(c)) {
+        		return new int[] {hm.get(c),i};
+        	}else hm.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("no match found");
+    }
 }
