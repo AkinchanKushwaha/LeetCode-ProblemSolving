c6e558deaf8ef1a88f77fa0fda5f0f6b47963844
AkinchanKushwaha <akinchandroger@gmail.com>
2020-10-22 11:42:51 +0530

    LeetCode

---
+++ LeetCode-ProblemSolving/src/SearchInsertPosition.java
@@ -0,0 +1,12 @@
+class SearchInsertPostion {
+    public int searchInsert(int[] nums, int target) {
+        for(int i = 0 ; i<nums.length; i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        if(nums[nums.length-1]<target){
+            return nums.length;
+        }else return 0;
+    }
+}
